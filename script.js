dd33fb4c0e1fc17f1e1fe46d34387f0f6a8298b4
Vincent <vincent854321@gmail.com>
2023-06-09 13:27:41 -0500

    Added player module and name collection :

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-let board = [];
+//Player Module
 
-function personFactory(name, age) {
-  function sayHello() {
-    console.log('hello!' + name);
-  }
-  
-  return ({
-    name, age, sayHello
-  });
-}
-let vince = personFactory("vincent",19);
+const Player = (() => {
+    function personFactory(name, piece) {
+        function sayHello() {
+            console.log('hello, im ' + name);
+        }
+
+        return ({
+            name, piece, sayHello,
+        });
+        
+    };
+    return{
+        personFactory,
+    };
+})();
+//Module for the name collection
+
+const input = (() => {
+    const start = document.querySelector("#start")
+    start.addEventListener("click", function (event) {
+        event.preventDefault();
+        const nameOne = document.getElementById("nameOne").value;
+        const nameTwo = document.getElementById("nameTwo").value;
+
+    })
+})();
+
+const game = (() => {
+    const board = [];
+    const gameBoard = document.createElement("div.class");
+})();
+
+
+
