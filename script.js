a7d2af2df1a85989e3635dc0516dc59f77b0b677
Vincent <vincent854321@gmail.com>
2023-06-11 14:48:49 -0500

    added functionallity to create player objs and display board

--- script.js
+++ script.js
@@ -1,36 +1,89 @@
-//Player Module
+//Function to create Players
+function personFactory(name, piece) {
+    function sayHello() {
+        console.log('hello, im ' + name);
+    }
 
-const Player = (() => {
-    function personFactory(name, piece) {
-        function sayHello() {
-            console.log('hello, im ' + name);
+    return ({
+        name, piece, sayHello,
+    });
+}
+//Module for gameBoard
+const gameboard = (() => {
+    // Private array to store the gameboard state
+    const board = ["", "", "", "", "", "", "", "", ""];
+
+    function displayBoard() {
+        const board = document.querySelector(".board");
+        const playerForm = document.querySelector(".player");
+        playerForm.classList.toggle("hidden");
+        board.classList.toggle("hidden");
+    }
+    // Public methods
+    const getBoard = () => board;
+
+    const updateCell = (index, marker) => {
+        if (board[index] === "") {
+            board[index] = marker;
+            return true; // Return true if the cell was successfully updated
         }
-
-        return ({
-            name, piece, sayHello,
-        });
-        
+        return false; // Return false if the cell is already occupied
     };
-    return{
-        personFactory,
+
+    const resetBoard = () => {
+        board.fill("");
+    };
+
+    // Return public methods
+    return {
+        getBoard,
+        updateCell,
+        resetBoard,
+        displayBoard,
     };
 })();
-//Module for the name collection
 
+
+//Module for the name collection and turn tracker
 const input = (() => {
+    let counter = 0;
+    const name = document.createElement("h1");
+    let nameOne, nameTwo;
+
     const start = document.querySelector("#start")
     start.addEventListener("click", function (event) {
         event.preventDefault();
-        const nameOne = document.getElementById("nameOne").value;
-        const nameTwo = document.getElementById("nameTwo").value;
-
+        nameOne = document.getElementById("nameOne").value;
+        nameTwo = document.getElementById("nameTwo").value;
+        gameboard.displayBoard();
     })
-})();
 
-const game = (() => {
-    const board = [];
-    const gameBoard = document.createElement("div.class");
-})();
+    const playerOne = personFactory(nameOne, "X");
+    const playerTwo = personFactory(nameTwo, "O");
 
+    const track = () => {
+        if (counter % 2 == 1) {
+            name.innerText = (playerOne);
+        } else {
+            name.innerText = (playerTwo);
+        }
+        counter++;
+
+    }
+    const addTracker = () => {
+        const container = document.createElement("div");
+        const body = document.querySelector(".container");
+        container.appendChild(name);
+        body.appendChild(container);
+    }
+
+
+    return {
+        track,
+        playerOne,
+        playerTwo,
+        addTracker,
+    }
+})();
 
 
