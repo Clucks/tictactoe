9cba7b5d119226e0af4589979e7878fdb442c0bf
Vincent <vincent854321@gmail.com>
2023-06-13 17:33:30 -0500

    added winner logic and play again button

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function personFactory(name, piece) {
     });
 }
 
-//Module for gameBoard
+//Module for gameBoard handles logic for winning
 const gameboard = (() => {
     // Private array to store the gameboard state
     const board = ["", "", "", "", "", "", "", "", ""];
@@ -47,10 +47,8 @@ const gameboard = (() => {
 })();
 
 
-//Module for input
+//Module for player form and start of game. 
 const playerForm = (() => {
-
-
     const start = document.querySelector("#start");
     start.addEventListener("click", function (event) {
         event.preventDefault();
@@ -75,7 +73,7 @@ const playerForm = (() => {
     };
 })();
 
-//Module for tracker
+//Module for game Flow 
 const gameFlow = (() => {
     let counter = 0;
     const name = document.createElement("h1");
@@ -102,16 +100,61 @@ const gameFlow = (() => {
         }
     };
 
+    const checkWinner = () => {
+        const board = gameboard.getBoard();
+        const winningConditions = [
+            // Define the winning conditions for tic-tac-toe
+            [0, 1, 2], // Row 1
+            [3, 4, 5], // Row 2
+            [6, 7, 8], // Row 3
+            [0, 3, 6], // Column 1
+            [1, 4, 7], // Column 2
+            [2, 5, 8], // Column 3
+            [0, 4, 8], // Diagonal from top-left to bottom-right
+            [2, 4, 6], // Diagonal from top-right to bottom-left
+        ];
+        for (let condition of winningConditions) {
+            const [a, b, c] = condition;
+            if (board[a] !== "" && board[a] === board[b] && board[a] === board[c]) {
+                // We have a winner
+                return true;
+            }
+        }
+    }
+
+    const createEnd = (piece) => {
+        const container = document.createElement("div");
+        const button = document.createElement("button");
+        button.innerText = "Play Again?";
+
+        container.appendChild(button);
+        container.style.gridColumn = "1 / 4";
+        container.style.gridRow = "1"; // Specify the row range as "1 / span 1" to occupy a single row
+        container.style.zIndex = "2";
+        container.style.backgroundColor = "rgba(255, 0, 0, 0.7)";
+
+
+        button.addEventListener("click", function (event) {
+
+        })
+        body.appendChild(container);
+    }
     const setInput = () => {
         const square = document.querySelectorAll(".square");
-        square.forEach(element => {
+        square.forEach((element, index) => {
             element.addEventListener("click", function (event) {
+                element.setAttribute("data-index", index);
                 if (element.innerText.trim().length === 0) {
                     const player = getTurn(playerOne, playerTwo);
                     console.log(player);
                     element.innerText = player.piece;
-                }
+                    const index = (event.target.getAttribute("data-index"));
+                    gameboard.updateCell(index, player.piece);
+                    if (checkWinner()) {
+                        createEnd(player.piece);
 
+                    }
+                }
             });
         })
     };
